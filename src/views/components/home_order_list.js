e64478a76d23bca6f3d1889528f0523958a44917
CalvinMoonly <calvin@moonlysoftware.com>
2023-01-07 19:29:35 +0100

    temp reverse

--- src/views/components/home_order_list.js
+++ src/views/components/home_order_list.js
@@ -3,7 +3,7 @@ const OrderList = ({ orders, current_orders, setCurrentCards }) => {
         <div>
             <div className='home_titles'>Orders ({orders.length})</div>
             <div className='orders_container'>
-                {orders.map((order, i) => {
+                {orders.reverse().map((order, i) => {
                     return (
                         <div onClick={() => setCurrentCards(order.id)} key={order.id} className={current_orders.includes(order.id) ? 'selected order_row' : 'order_row'}>
                             <div className='order_row_content'>
